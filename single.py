70d6c63d477b087fb5a9debdd75e1d002356574a
nasseralbess <nasseralbess98@gmail.com>
2025-03-03 19:57:08 +0300

    First commit

---
+++ single.py
@@ -0,0 +1,10 @@
+import torch
+from torch.utils.data import DataLoader, Dataset
+class Trainer:
+    def __init__(self, model: torch.nn.Module, train_data: DataLoader, optimizer: torch.optim.Optimizer, gpu_id: int, save_every: int):
+        self.gpu_id = gpu_id
+        self.model = model.to(gpu_id)
+        self.train_data = train_data
+        self.optimizer = optimizer
+        self.save_every = save_every
+        
