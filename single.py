54f7e61a59fc0a1571fbfe314abf329e5e23811c
nasseralbess <nasseralbess98@gmail.com>
2025-03-03 20:15:24 +0300

    commit

--- single.py
+++ single.py
@@ -8,3 +8,30 @@ class Trainer:
         self.optimizer = optimizer
         self.save_every = save_every
         
+    def _run_batch(self, source, targets):
+        self.optimizer.zero_grad()
+        out = self.model(source)
+        loss = torch.nn.CrossEntropyLoss()(out, targets)
+        loss.backward()
+        self.optimizer.step()      
+    
+    def _run_epoch(self, epoch):
+        batch_size = len(next(iter(self.train_data))[0])
+        print(f"[GPU{self.gpu_id}] Epoch {epoch} | Batchsize: {batch_size} | Steps: {len(self.train_data)}")
+        for source, targets in self.train_data:
+            source = source.to(self.gpu_id)
+            targets = targets.to(self.gpu_id)
+            self._run_batch(source, targets)
+    
+    def _save_checkpoint(self, epoch):
+        checkpoint = self.model.state_dict()
+        torch.save(checkpoint, "checkpoint.pt")
+        print(f"Epoch {epoch} | chckpoin saved at checkpoint.pt")
+        
+    def train(self, max_epochs: int):
+        for epoch in range(max_epochs):
+            self._run_epoch(epoch)
+            if epoch % self.save_every == 0:
+                self._save_checkpoint(epoch)
+    
+# def  
