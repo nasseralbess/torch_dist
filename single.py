21bfbab76b76ccbbd97686f8713ad6b73b639797
nasseralbess <nasseralbess98@gmail.com>
2025-03-04 17:13:43 +0300

    single

--- single.py
+++ single.py
@@ -1,5 +1,17 @@
 import torch
 from torch.utils.data import DataLoader, Dataset
+
+class MyTrainDataset(Dataset):
+    def __init__(self, size):
+        self.size = size
+        self.data = [(torch.rand(20), torch.rand(1)) for _ in range(size)]
+
+    def __len__(self):
+        return self.size
+    
+    def __getitem__(self, index):
+        return self.data[index]
+    
 class Trainer:
     def __init__(self, model: torch.nn.Module, train_data: DataLoader, optimizer: torch.optim.Optimizer, gpu_id: int, save_every: int):
         self.gpu_id = gpu_id
@@ -26,7 +38,7 @@ class Trainer:
     def _save_checkpoint(self, epoch):
         checkpoint = self.model.state_dict()
         torch.save(checkpoint, "checkpoint.pt")
-        print(f"Epoch {epoch} | chckpoin saved at checkpoint.pt")
+        print(f"Epoch {epoch} | checkpoint saved at checkpoint.pt")
         
     def train(self, max_epochs: int):
         for epoch in range(max_epochs):
@@ -34,4 +46,24 @@ class Trainer:
             if epoch % self.save_every == 0:
                 self._save_checkpoint(epoch)
     
-# def  
+def  load_train_objs():
+    train_set = MyTrainDataset(2048)
+    model = torch.nn.Linear(20,1)
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+    return train_set, model, optimizer
+
+def prepare_dataloader(dataset: Dataset, batch_size: int):
+    return DataLoader(dataset, batch_size=batch_size, pin_memory=True, shuffle=True)
+
+def main(device, total_epochs, save_every):
+    dataset, model, optimizer = load_train_objs()
+    train_data = prepare_dataloader(dataset, batch_size=32)
+    trainer = Trainer(model, train_data, optimizer, device, save_every)
+    trainer.train(total_epochs)
+
+if __name__=="__main__":
+    import sys
+    total_epochs = int(sys.argv[1])
+    save_every = int(sys.argv[2])
+    device = 0
+    main(device, total_epochs, save_every)
