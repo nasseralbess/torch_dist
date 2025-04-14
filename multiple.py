b00d98ed55a639951c37745d7b68ea9330488eaa
nasseralbess <nasseralbess98@gmail.com>
2025-04-14 19:48:15 +0300

    Multiple

--- multiple.py
+++ multiple.py
@@ -1,11 +1,22 @@
 import torch
 from torch.utils.data import DataLoader, Dataset
+import torch.multiprocessing as mp
+from torch.utils.data.distributed import DistributedSampler
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.distributed import init_process_group, destroy_process_group
+import os
+
+def ddp_setup(rank, world_size):
+    os.environ["MASTER_ADDR"]="0.0.0.0"
+    os.environ["MASTER_PORT"]= "12355"
+    init_process_group(backend="nccl", rank=rank, world_size=world_size)
+
 
 class MyTrainDataset(Dataset):
     def __init__(self, size):
         self.size = size
         self.data = [(torch.rand(20), torch.rand(1)) for _ in range(size)]
-
+        self.model = DDP(self.model, device_ids=[self.gpu_id])
     def __len__(self):
         return self.size
     
@@ -36,14 +47,14 @@ class Trainer:
             self._run_batch(source, targets)
     
     def _save_checkpoint(self, epoch):
-        checkpoint = self.model.state_dict()
+        checkpoint = self.model.module.state_dict()
         torch.save(checkpoint, "checkpoint.pt")
         print(f"Epoch {epoch} | checkpoint saved at checkpoint.pt")
         
     def train(self, max_epochs: int):
         for epoch in range(max_epochs):
             self._run_epoch(epoch)
-            if epoch % self.save_every == 0:
+            if self.gpu_id = 0 and epoch % self.save_every == 0:
                 self._save_checkpoint(epoch)
     
 def  load_train_objs():
@@ -53,18 +64,23 @@ def  load_train_objs():
     return train_set, model, optimizer
 
 def prepare_dataloader(dataset: Dataset, batch_size: int):
-    return DataLoader(dataset, batch_size=batch_size, pin_memory=True, shuffle=True)
+    return DataLoader(dataset, batch_size=batch_size, pin_memory=True, shuffle=False, sampler=DistributedSampler(dataset)
 
-def main(device, total_epochs, save_every):
+def main(rank:int, world_size:int total_epochs:int, save_every:int):
+    ddp_setup(rank, world_size)
     dataset, model, optimizer = load_train_objs()
     train_data = prepare_dataloader(dataset, batch_size=32)
-    trainer = Trainer(model, train_data, optimizer, device, save_every)
+    trainer = Trainer(model, train_data, optimizer, rank, save_every)
     trainer.train(total_epochs)
+    destroy_process_group()
+
 
 if __name__=="__main__":
     import sys
     total_epochs = int(sys.argv[1])
     save_every = int(sys.argv[2])
-    #device = 0
-    device = 'cpu'
+    device = 0
+    #device = 'cpu'
+    world_size = 2
+    mp.spawn(main,args=(world_size,total_epochs, save_every), nprocs=world_size)
     main(device, total_epochs, save_every)
